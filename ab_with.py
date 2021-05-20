40ffdae90d593f25410c083e50f5935ebebf88b4
Selik@kone <konnehjonny@gmail.com>
2021-05-20 04:04:41 +0300

    Changed the file name to file.txt intseal of data.txtx

--- ab_with.py
+++ ab_with.py
@@ -1,6 +1,6 @@
 #this example shows the use of "with" keyword for automatically closing files
 a,b = 0,0
-FILENAME = "data.txt"
+FILENAME = "file.txt"
 
 while 1:
 
@@ -19,10 +19,10 @@ while 1:
     option = input("Option: ").upper()
 
     if option == "A":
-        a = int(input("enter new value for A:"))
+        a = int(input("enter value for A:"))
 
     elif option == "B":
-        b = int(input("enter new value for B:"))
+        b = int(input("enter value for B:"))
 
     elif option == "S":
         with open(FILENAME, "w") as f:
